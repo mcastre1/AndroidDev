7f9a0aff66b8861c47a54ff217ca329eb8210007
mcastre1 <miguelcastrejongal@gmail.com>
2023-11-17 21:32:36 -0700

    Remember, mutablestateof, and expanding column

--- BasicsCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ BasicsCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,38 +33,56 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(modifier: Modifier = Modifier){
+fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("World", "Compose")){
     Surface(
         modifier = modifier,
         color = MaterialTheme.colorScheme.background
     ) {
-        Greeting(name = "Android")
+        Column(modifier.padding()) {
+            for (name in names) {
+                Greeting(name = "$name")
+            }
+        }
+
     }
 
 }
 
 @Composable
 fun Greeting(name: String) {
+    // This is how we save a value for each recomposition
+    // We use mutablestatof to tell compose to recompose whenever this value changes
+    // remember is used to compose remembers the new value whenever recomposition happens
+    // again.
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
     // Surfaces are like a box around all composable objects inside its body
     // The advantage is that it will know when to use light or dark mode when
     // compared to a, lets say, box.
     Surface (
-        color = MaterialTheme.colorScheme.primary
-    ){
-        Column (
-            modifier = Modifier.padding(24.dp)
-        ){
-            Text(
-                text = "Hello,",
-            )
-            Text(
-                text = "$name!",
-            )
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier.weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(
+                    text = "Hello,",
+                )
+                Text(
+                    text = "$name!",
+                )
+            }
+            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     BasicsCodelabTheme {
